197dad8ae3b3aea8b11cc57dc115a391dbbb8bc5
catlin42 <catherinelin42@gmail.com>
2025-01-08 23:30:46 -0500

    try to fix database

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask,render_template,redirect,request
 from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] =\
-        'sqlite:///projects.sqlite'
+        'sqlite:///projects.db'
 
 db = SQLAlchemy(app)
+
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(1000), nullable=False)
@@ -14,6 +16,8 @@ class Project(db.Model):
 
 @app.route('/',methods=('GET', 'POST'))
 def index():
+    with app.app_context():
+        db.create_all()
     projects = Project.query.all()
     if request.method == 'POST':
         title = request.form['title']
